4a6176d3e6f004b3dedf40aa1372833296959c77
tram240505 <trampham2405@gmail.com>
2025-04-10 21:38:25 -0400

    add ccomment

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -5,17 +5,23 @@ import styles from '../styles/HomePage.module.css';
 function Home(){
     const router = useRouter();
 
+    // Function to get a random recipe
     const getRandomRecipe = () => {
         const timestamp = Date.now();
         router.push(`/ResultPage?type=random&time=${timestamp}`); 
     };
 
+    // Function to navigate to the custom dish search page
     const customRecipe = () => {
         router.push("/SearchPage"); 
     };
+
+    // Function to navigate to the favorite dishes page
     const goToFavorites = () => {
         router.push("/FavoriteDishesPage");
     };
+
+    // Function to scroll to the top of the page
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: "smooth" });
     };
@@ -23,7 +29,6 @@ function Home(){
 
     return(
         <div>
-            {/* Navbar */}
             <nav className={styles.navbar}>
                 <button onClick={scrollToTop}>Home</button>
                 <button onClick={goToFavorites}>Favorite Dishes</button>
@@ -32,8 +37,8 @@ function Home(){
                 <h1>WHAT TO EAT?</h1>
             </div>
             <div>
-                <button onClick={getRandomRecipe}>Get Random Dish</button>
-                <button onClick={customRecipe}>Customize Your Dish</button>
+                <button onClick={getRandomRecipe}>Get Random Dish</button> {/* Fetch random dish*/}
+                <button onClick={customRecipe}>Customize Your Dish</button> {/*Navigate to dish customization pagepage*/}
             </div>
         </div>
     )
